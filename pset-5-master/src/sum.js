0735f262b5627f23806fbb30a41acd421d8d5ec0
tikano <oadmathgenie@gmail.com>
2019-11-29 21:17:45 -0500

    sum

--- pset-5-master/src/sum.js
+++ pset-5-master/src/sum.js
@@ -3,50 +3,22 @@ const readlineSync = require("readline-sync");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-let endLoop = false;
-let lowerBound = null;
-let upperBound = null;
+let lowerbound = null;
+let upperbound = null;
+let sum = 0;
 
 console.log();
 
-while (endLoop === false) {
-    let lowerInput = readlineSync.question("Lower bound: ");
-    let upperInput = readlineSync.question("Upper bound: ");
-    lowerBound = Number(lowerInput);
-    upperBound = Number(upperInput);
-
-    if (Number.isNaN(lowerBound) || Number.isNaN(upperBound)) {
-    }
-    else if (lowerInput === "" || upperInput === "") {
-    }
-    else if (lowerBound % 1 !== 0 || upperBound % 1 !== 0) {
-    }
-    else if (lowerBound < MIN || lowerBound > MAX || upperBound < MIN || upperBound > MAX) {
-    }
-    else if (lowerBound >= upperBound) {
-    }
-    else {
-        endLoop = true;
-    }
+while (lowerbound % 1 !== 0 || upperbound % 1 !== 0 || lowerbound < MIN || upperbound > MAX || lowerbound >= upperbound) {
+    lowerbound = Number(readlineSync.question("Lower bound: "));
+    upperbound = Number(readlineSync.question("Upper bound: "));
 }
 
-let lowerEvenBound = null;
-let upperEvenBound = null;
-
-if (lowerBound % 2 === 0) {
-    lowerEvenBound = lowerBound;
-}
-else {
-    lowerEvenBound = lowerBound + 1;
-}
-if (upperBound % 2 === 0) {
-    upperEvenBound = upperBound;
-}
-else {
-    upperEvenBound = upperBound - 1;
+for(let i = lowerbound; i <= upperbound; i++){
+  if(i % 2 == 0){
+    sum = sum + i;
+  }
 }
 
-const NUMBER_OF_EVENS = ((upperEvenBound - lowerEvenBound) / 2) + 1;
-const SUM = (NUMBER_OF_EVENS / 2) * (lowerEvenBound + upperEvenBound);
-const SUM_WITH_COMMAS = SUM.toLocaleString("en");
-console.log("\n" + SUM_WITH_COMMAS + ".\n");
+const formattedsum = sum.toLocaleString("en");
+console.log("\n" + formattedsum + ".\n");
